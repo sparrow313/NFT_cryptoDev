b9187b7bd4c822db678f492c482b3e35b5403076
@javedakeeb1994 <javedakeeb1994@mail.com>
2022-12-01 14:59:15 +0530

    new contract address

--- hardhat-tutorial/constants/index.js
+++ hardhat-tutorial/constants/index.js
@@ -1,5 +1,4 @@
-const WHITELIST_CONTRACT_ADDRESS =
-  "0x951f179Dd7DE72a1ec428Dccb39c7804A189604C0";
-const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+const WHITELIST_CONTRACT_ADDRESS = "0x951f179Dd7DE72a1ec428Dccb39c7804A189604C";
+const METADATA_URL = "https://nft-crypto-dev-seven.vercel.app/api/";
 
 module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
